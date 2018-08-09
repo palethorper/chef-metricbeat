75c5fbd01c56da57dc66302d97dbfe8d2bf432e6
Robert Palethorpe <robert.palethorpe@itaas.dimensiondata.com>
2018-08-10 02:20:44 +1000

    Bumping version and linting to add chef_version & depenadancies

--- metadata.rb
+++ metadata.rb
@@ -1,21 +1,23 @@
 name 'metricbeat'
+
 maintainer 'Virender Khatri'
 maintainer_email 'vir.khatri@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures Elastic metricbeat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
-
-chef_version '~> 12'
+version '0.2.11'
 
 source_url 'https://github.com/vkhatri/chef-metricbeat' if respond_to?(:source_url)
 issues_url 'https://github.com/vkhatri/chef-metricbeat/issues' if respond_to?(:issues_url)
+
+chef_version '>= 12.14' if respond_to?(:chef_version)
 
 depends 'yum'
 depends 'apt'
 depends 'windows'
 depends 'yum-plugin-versionlock'
+depends 'elastic_beats_repo'
 
-%w[ubuntu centos amazon redhat fedora windows].each do |os|
+%w(ubuntu centos amazon redhat fedora windows).each do |os|
   supports os
 end
